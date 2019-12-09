b3e6cb57e4a563823db53eeb6f88efa90517990f
aiya000 <aiya000.develop@gmail.com>
2019-12-09 16:15:10 +0900

    :+1: Add a plural id for tests

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,28 +2,28 @@
   <div :class="$style.container">
       <input type="text" v-model="result" :class="result" />
       <div>
-          <button onclick="resolve('1')">1</button>
-          <button onclick="resolve('2')">2</button>
-          <button onclick="resolve('3')">3</button>
-          <button onclick="resolve('+')">+</button>
+          <button id="button-1"   @click="resolve('1')">1</button>
+          <button id="button-2"   @click="resolve('2')">2</button>
+          <button id="button-3"   @click="resolve('3')">3</button>
+          <button id="button-add" @click="resolve('+')">+</button>
       </div>
       <div>
-          <button onclick="resolve('4')">4</button>
-          <button onclick="resolve('5')">5</button>
-          <button onclick="resolve('6')">6</button>
-          <button onclick="resolve('-')">-</button>
+          <button id="button-4"   @click="resolve('4')">4</button>
+          <button id="button-5"   @click="resolve('5')">5</button>
+          <button id="button-6"   @click="resolve('6')">6</button>
+          <button id="button-sub" @click="resolve('-')">-</button>
       </div>
       <div>
-          <button onclick="resolve('7')">7</button>
-          <button onclick="resolve('8')">8</button>
-          <button onclick="resolve('9')">9</button>
-          <button onclick="resolve('*')">*</button>
+          <button id="button-7"     @click="resolve('7')">7</button>
+          <button id="button-8"     @click="resolve('8')">8</button>
+          <button id="button-9"     @click="resolve('9')">9</button>
+          <button id="button-times" @click="resolve('*')">*</button>
       </div>
       <div>
-          <button onclick="resolve('C')">C</button>
-          <button onclick="resolve('0')">0</button>
-          <button onclick="resolve('x')">x</button>
-          <button onclick="resolve('/')">/</button>
+          <button id="button-clear" @click="resolve('C')">C</button>
+          <button id="button-0"     @click="resolve('0')">0</button>
+          <button id="button-apply" @click="resolve('x')">x</button>
+          <button id="button-div"   @click="resolve('/')">/</button>
       </div>
   </div>
 </template>
@@ -38,7 +38,8 @@ export default class Calculator extends Vue {
   @Prop({ default: 0 })
   public readonly result!: number
 
-  public resolve(pushed: ButtonType): void {
+  public resolve(pushed: ButtonType): () => void {
+    return () => {}
   }
 }
 
