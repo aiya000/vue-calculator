569176c28832e1081cd464dc65fe3e25e1d82b0b
aiya000 <aiya000.develop@gmail.com>
2019-12-09 13:08:44 +0900

    :+1: Disable a lint rule 'space-before-function-paren'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript'
+    '@vue/typescript',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)'
+        '**/__tests__/*.{j,t}s?(x)',
       ],
       env: {
         mocha: true
-      }
-    }
-  ]
+      },
+    },
+  ],
 }
