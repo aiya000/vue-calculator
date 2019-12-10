a1782ab732c6fa300d457837f73fb7ec26326bf0
aiya000 <aiya000.develop@gmail.com>
2019-12-10 10:48:14 +0900

    :up: Use devtools

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import Vue from 'vue'
+import * as DevTools from '@vue/devtools'
 import App from './App.vue'
+import Vue from 'vue'
 
 Vue.config.productionTip = false
+
+if (process.env.NODE_ENV === 'development') {
+  DevTools.connect('localhost', 8098)
+}
 
 new Vue({
   render: h => h(App)
