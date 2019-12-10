db7eff89d8bd3ddf23aa146a2368864ebfad5bfd
aiya000 <aiya000.develop@gmail.com>
2019-12-10 10:39:25 +0900

    :+1: Ignore comma-dangle

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'space-before-function-paren': 'off',
+    'comma-dangle': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
